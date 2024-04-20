50f3e3aae781c93a7a060139f13e30e3026a9b80
caosl <caosl@uci.edu>
2024-04-20 03:37:47 -0700

    revise storeValue function

--- functions/test.py
+++ functions/test.py
@@ -1,13 +1,19 @@
 import json
+import uuid
 
 
 
+def storeValue(parameter: dict):
+    # store the parameter dict containing name and birthday in a json file as uuid
 
-def storeValue(parameter):
     with open('user.json', 'r+') as json_file:
         json_data         = json.load(json_file)
-        json_data['user'] = parameter
+        myuuid            = uuid.uuid4()
+        json_data[myuuid] = parameter
 
         json_file.seek(0)
         json.dump(json_data, json_file, indent=4) 
         json_file.truncate()
+
+    return myuuid
+
