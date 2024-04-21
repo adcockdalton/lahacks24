73b2549f6b34737951a8589464ecbadb564234b2
caosl <caosl@uci.edu>
2024-04-21 03:38:09 -0700

    create component for eventcard

--- client/frontend/src/components/ui/information_box.jsx
+++ client/frontend/src/components/ui/information_box.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './CardComponents';
 
-const EventCard = ({ key, title, description, county }) => {
+const EventCard = ({ response }) => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/getRelatedEvent', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.error(error));
+    }, []);
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <Card className="m-4">
             <CardHeader>
-                <CardTitle>{title}</CardTitle>
+                <CardTitle>{data.title}</CardTitle>
             </CardHeader>
             <CardContent>
-                <CardDescription>{description}</CardDescription>
+                <CardDescription>{data.description}</CardDescription>
             </CardContent>
             <CardFooter>
-                {county}
+                {data.county}
             </CardFooter>
         </Card>
     );
